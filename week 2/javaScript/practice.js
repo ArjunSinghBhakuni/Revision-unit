d3fd4f4f3455ee90bdc2adc563e1bff0b807448b
ArjunSinghBhakuni <arjunbhakuni23@gmail.com>
2022-09-20 10:01:06 +0530

    revised of js questions

---
+++ week 2/javaScript/practice.js
@@ -0,0 +1,19 @@
+
+// function a(){
+//  var x = 100
+//  this.b()
+// }
+
+// function b(){
+//  var x = 200
+//  console.log(this.x)
+// }
+// a()
+
+// const sum =(x,y)=>{
+//  x = x && 5
+//  y = y || 5
+//  return x+y
+// }
+
+// console.log(sum(20,5))
