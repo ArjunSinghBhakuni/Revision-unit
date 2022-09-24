d33fa06cb7bd346ade91a835b5e9347a721b92dd
ArjunSinghBhakuni <arjunbhakuni23@gmail.com>
2022-09-25 00:05:09 +0530

    Js 2 revision done

--- javaScript/js2.js
+++ javaScript/js2.js
@@ -57,3 +57,29 @@ var arr = [1, 2, 3, 4, 5];
 // DSA
 // input => 'key,value,pair,javascript'
 // output => {key:{value:{pair:{javascript:{}}}}}
+
+let str = "key,value,pair,javascript"
+
+// for loop
+var arr  = str.split(",")
+// let obj = {}
+// obj[arr[arr.length-1]] = {}
+// for(let i =arr.length-2;i>=0;i--){
+ 
+//  obj[arr[i]] = {...obj}
+//  delete obj[arr[i+1]]
+// }
+// console.log(obj)
+
+//Recursion
+// let i =0
+// console.log(rec(i,arr))
+// function rec(i,arr){
+
+//  if(i === arr.length){
+//   return {}
+//  } else {
+// return { [arr[i]]:rec(i+1,arr)}
+//  }
+// }
+
